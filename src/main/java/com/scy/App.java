943c2897be2d143315d93eb16ac940fe91d9d92a
shy <2776441252@qq.com>
2022-01-03 20:36:29 +0800

    第一次提交

--- src/main/java/com/scy/App.java
+++ src/main/java/com/scy/App.java
@@ -1,5 +1,6 @@
 package com.scy;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -12,6 +13,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/scy/controller/first.java
+++ src/main/java/com/scy/controller/first.java
@@ -1,4 +1,4 @@
-package controller;
+package com.scy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
